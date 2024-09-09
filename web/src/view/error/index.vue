c16be258f10dae1f0d9566db1db3912e48f5e66f
rightsun <sunsq@hello-tech.com>
2024-09-09 16:28:41 +0800

    merge 冲突

--- web/src/view/error/index.vue
+++ web/src/view/error/index.vue
@@ -1,12 +1,20 @@
-<<<<<<< HEAD
 <template>
   <div>
-    <div class="w-full h-screen bg-gray-50 flex items-center justify-center">
-      <div class="flex flex-col items-center text-2xl gap-4">
-        <img class="w-1/3" src="../../assets/404.png">
+    <div class="flex items-center justify-center w-full h-screen bg-gray-50">
+      <div class="flex flex-col items-center gap-4 text-2xl">
+        <img class="w-1/3" src="../../assets/404.png" />
         <p class="text-lg">页面被神秘力量吸走了</p>
-        <p class="text-lg">常见问题为当前此角色无当前路由，如果确定要使用本路由，请到角色管理进行分配</p>
-        <p>项目地址：<a href="https://github.com/flipped-aurora/gin-vue-admin" target="_blank" class="text-blue-600  ">https://github.com/flipped-aurora/gin-vue-admin</a></p>
+        <p class="text-lg">
+          常见问题为当前此角色无当前路由，如果确定要使用本路由，请到角色管理进行分配
+        </p>
+        <p>
+          项目地址：<a
+            href="https://github.com/flipped-aurora/gin-vue-admin"
+            target="_blank"
+            class="text-blue-600"
+            >https://github.com/flipped-aurora/gin-vue-admin</a
+          >
+        </p>
         <el-button @click="toDashboard">返回首页</el-button>
       </div>
     </div>
@@ -18,7 +26,7 @@ import { useUserStore } from '@/pinia/modules/user'
 import { useRouter } from 'vue-router'
 
 defineOptions({
-  name: 'Error'
+  name: 'Error',
 })
 
 const userStore = useUserStore()
@@ -27,37 +35,3 @@ const toDashboard = () => {
   router.push({ name: userStore.userInfo.authority.defaultRouter })
 }
 </script>
-=======
-<template>
-  <div>
-    <div class="w-full h-screen bg-gray-50 flex items-center justify-center">
-      <div class="flex flex-col items-center text-2xl gap-4">
-        <img src="../../assets/notFound.png">
-        <p>页面被神秘力量吸走了（如果您是开源版请联系我们修复）</p>
-        <p style="font-size:18px;line-height:40px;">常见问题为当前此角色无当前路由，如果确定要使用本路由，请到角色管理进行分配</p>
-        <p>↓</p>
-        <img
-          src="../../assets/qm.png"
-          class="w-16 h-16 mt-20"
-        >
-        <el-button @click="toDashboard">返回首页</el-button>
-      </div>
-    </div>
-  </div>
-</template>
-
-<script setup>
-import { useUserStore } from '@/pinia/modules/user'
-import { useRouter } from 'vue-router'
-
-defineOptions({
-  name: 'Error'
-})
-
-const userStore = useUserStore()
-const router = useRouter()
-const toDashboard = () => {
-  router.push({ name: userStore.userInfo.authority.defaultRouter })
-}
-</script>
->>>>>>> wx-tally
--- web/src/view/layout/header/index.vue
+++ web/src/view/layout/header/index.vue
@@ -1,57 +1,69 @@
-<<<<<<< HEAD
 <!--
     @auther: bypanghu<bypanghu@163.com>
     @date: 2024/5/7
 !-->
 
 <template>
-  <div class="flex justify-between fixed top-0 left-0 right-0 z-10 h-16 bg-white text-slate-700 dark:text-slate-300  dark:bg-slate-900 shadow dark:shadow-gray-700 flex items-center px-2">
-    <div
-      class="flex items-center cursor-pointer flex-1"
-    >
-      <div class="flex items-center cursor-pointer min-w-48" @click="router.push({ path: '/' })">
+  <div
+    class="fixed top-0 left-0 right-0 z-10 flex items-center justify-between h-16 px-2 bg-white shadow text-slate-700 dark:text-slate-300 dark:bg-slate-900 dark:shadow-gray-700"
+  >
+    <div class="flex items-center flex-1 cursor-pointer">
+      <div
+        class="flex items-center cursor-pointer min-w-48"
+        @click="router.push({ path: '/' })"
+      >
         <img
           alt
           class="h-12 bg-white rounded-full"
           :src="$GIN_VUE_ADMIN.appLogo"
-        >
+        />
         <div
           v-if="!isMobile"
-          class="inline-flex font-bold text-2xl ml-2"
-          :class="(config.side_mode === 'head' || config.side_mode === 'combination') &&'min-w-fit'"
+          class="inline-flex ml-2 text-2xl font-bold"
+          :class="
+            (config.side_mode === 'head' ||
+              config.side_mode === 'combination') &&
+            'min-w-fit'
+          "
         >
           {{ $GIN_VUE_ADMIN.appName }}
         </div>
       </div>
 
-      <el-breadcrumb v-show="!isMobile" v-if="config.side_mode !== 'head'&& config.side_mode !== 'combination'" class="ml-4">
+      <el-breadcrumb
+        v-show="!isMobile"
+        v-if="config.side_mode !== 'head' && config.side_mode !== 'combination'"
+        class="ml-4"
+      >
         <el-breadcrumb-item
           v-for="item in matched.slice(1, matched.length)"
           :key="item.path"
         >
-          {{
-            fmtTitle(item.meta.title, route)
-          }}
+          {{ fmtTitle(item.meta.title, route) }}
         </el-breadcrumb-item>
       </el-breadcrumb>
-      <gva-aside v-if="config.side_mode === 'head' && !isMobile" class="flex-1" />
-      <gva-aside v-if="config.side_mode === 'combination' && !isMobile" mode="head" class="flex-1" />
+      <gva-aside
+        v-if="config.side_mode === 'head' && !isMobile"
+        class="flex-1"
+      />
+      <gva-aside
+        v-if="config.side_mode === 'combination' && !isMobile"
+        mode="head"
+        class="flex-1"
+      />
     </div>
 
-    <div class="ml-2 flex items-center">
+    <div class="flex items-center ml-2">
       <tools />
       <el-dropdown>
-        <div
-          class="flex justify-center items-center h-full w-full"
-        >
+        <div class="flex items-center justify-center w-full h-full">
           <span
-            class="cursor-pointer flex justify-center items-center text-black dark:text-gray-100"
+            class="flex items-center justify-center text-black cursor-pointer dark:text-gray-100"
           >
             <CustomPic />
-            <span
-              v-show="!isMobile"
-              class="w-16"
-            >{{ userStore.userInfo.nickName }}</span>
+            <span v-show="!isMobile" class="w-16">{{
+              userStore.userInfo.nickName
+            }}</span>
             <el-icon>
               <arrow-down />
             </el-icon>
@@ -61,33 +73,24 @@
           <el-dropdown-menu>
             <el-dropdown-item>
               <span class="font-bold">
-                当前角色：{{
-                  userStore.userInfo.authority.authorityName
-                }}
+                当前角色：{{ userStore.userInfo.authority.authorityName }}
               </span>
             </el-dropdown-item>
             <template v-if="userStore.userInfo.authorities">
               <el-dropdown-item
                 v-for="item in userStore.userInfo.authorities.filter(
-                  (i) =>
-                    i.authorityId !==
-                    userStore.userInfo.authorityId
+                  (i) => i.authorityId !== userStore.userInfo.authorityId
                 )"
                 :key="item.authorityId"
                 @click="changeUserAuth(item.authorityId)"
               >
-                <span>
-                  切换为：{{ item.authorityName }}
-                </span>
+                <span> 切换为：{{ item.authorityName }} </span>
               </el-dropdown-item>
             </template>
             <el-dropdown-item icon="avatar" @click="toPerson">
               个人信息
             </el-dropdown-item>
-            <el-dropdown-item
-              icon="reading-lamp"
-              @click="userStore.LoginOut"
-            >
+            <el-dropdown-item icon="reading-lamp" @click="userStore.LoginOut">
               登 出
             </el-dropdown-item>
           </el-dropdown-menu>
@@ -98,186 +101,39 @@
 </template>
 
 <script setup>
-import  tools from "./tools.vue"
+import tools from './tools.vue'
 import CustomPic from '@/components/customPic/index.vue'
-import { useUserStore } from "@/pinia/modules/user";
+import { useUserStore } from '@/pinia/modules/user'
 import { useRoute, useRouter } from 'vue-router'
-import { useAppStore } from "@/pinia"
-import { storeToRefs } from "pinia"
-import { computed, } from 'vue'
+import { useAppStore } from '@/pinia'
+import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
 import { setUserAuthority } from '@/api/user'
-import { fmtTitle } from "@/utils/fmtRouterTitle";
-import gvaAside from "@/view/layout/aside/index.vue"
-const userStore = useUserStore();
+import { fmtTitle } from '@/utils/fmtRouterTitle'
+import gvaAside from '@/view/layout/aside/index.vue'
+const userStore = useUserStore()
 const router = useRouter()
 const route = useRoute()
 const appStore = useAppStore()
-const { device , config } = storeToRefs(appStore)
-const isMobile = computed(() =>{
-  return device.value  === 'mobile'
+const { device, config } = storeToRefs(appStore)
+const isMobile = computed(() => {
+  return device.value === 'mobile'
 })
 const toPerson = () => {
-  router.push({ name: "person" });
-};
-const matched = computed(() => route.meta.matched);
-
+  router.push({ name: 'person' })
+}
+const matched = computed(() => route.meta.matched)
 
 const changeUserAuth = async (id) => {
   const res = await setUserAuthority({
     authorityId: id,
-  });
+  })
   if (res.code === 0) {
-    window.sessionStorage.setItem("needCloseAll", "true");
-    window.sessionStorage.setItem("needToHome", "true");
-    window.location.reload();
+    window.sessionStorage.setItem('needCloseAll', 'true')
+    window.sessionStorage.setItem('needToHome', 'true')
+    window.location.reload()
   }
-};
-
-
+}
 </script>
 
-<style scoped lang="scss">
-
-</style>
-=======
-<!--
-    @auther: bypanghu<bypanghu@163.com>
-    @date: 2024/5/7
-!-->
-
-<template>
-  <div class="flex justify-between fixed top-0 left-0 right-0 z-10 h-16 bg-white text-slate-700 dark:text-slate-300  dark:bg-slate-900 shadow dark:shadow-gray-700 flex items-center px-2">
-    <div
-      class="flex items-center cursor-pointer flex-1"
-    >
-      <div class="flex items-center cursor-pointer" @click="router.push({ path: '/' })">
-        <img
-          alt
-          class="h-12 bg-white rounded-full"
-          :src="$GIN_VUE_ADMIN.appLogo"
-        >
-        <div
-          v-if="!isMobile"
-          class="inline-flex font-bold text-2xl ml-2"
-          :class="(config.side_mode === 'head' || config.side_mode === 'combination') &&'min-w-fit'"
-        >
-          {{ $GIN_VUE_ADMIN.appName }}
-        </div>
-      </div>
-
-      <el-breadcrumb v-show="!isMobile" v-if="config.side_mode !== 'head'&& config.side_mode !== 'combination'" class="ml-4">
-        <el-breadcrumb-item
-          v-for="item in matched.slice(1, matched.length)"
-          :key="item.path"
-        >
-          {{
-            fmtTitle(item.meta.title, route)
-          }}
-        </el-breadcrumb-item>
-      </el-breadcrumb>
-      <gva-aside v-if="config.side_mode === 'head' && !isMobile" class="flex-1" />
-      <gva-aside v-if="config.side_mode === 'combination' && !isMobile" mode="head" class="flex-1" />
-    </div>
-
-    <div class="ml-2 flex items-center">
-      <tools />
-      <el-dropdown>
-        <div
-          class="flex justify-center items-center h-full w-full"
-        >
-          <span
-            class="cursor-pointer flex justify-center items-center text-black dark:text-gray-100"
-          >
-            <CustomPic />
-            <span
-              v-show="!isMobile"
-              class="w-16"
-            >{{ userStore.userInfo.nickName }}</span>
-            <el-icon>
-              <arrow-down />
-            </el-icon>
-          </span>
-        </div>
-        <template #dropdown>
-          <el-dropdown-menu>
-            <el-dropdown-item>
-              <span class="font-bold">
-                当前角色：{{
-                  userStore.userInfo.authority.authorityName
-                }}
-              </span>
-            </el-dropdown-item>
-            <template v-if="userStore.userInfo.authorities">
-              <el-dropdown-item
-                v-for="item in userStore.userInfo.authorities.filter(
-                  (i) =>
-                    i.authorityId !==
-                    userStore.userInfo.authorityId
-                )"
-                :key="item.authorityId"
-                @click="changeUserAuth(item.authorityId)"
-              >
-                <span>
-                  切换为：{{ item.authorityName }}
-                </span>
-              </el-dropdown-item>
-            </template>
-            <el-dropdown-item icon="avatar" @click="toPerson">
-              个人信息
-            </el-dropdown-item>
-            <el-dropdown-item
-              icon="reading-lamp"
-              @click="userStore.LoginOut"
-            >
-              登 出
-            </el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
-    </div>
-  </div>
-</template>
-
-<script setup>
-import  tools from "./tools.vue"
-import CustomPic from '@/components/customPic/index.vue'
-import { useUserStore } from "@/pinia/modules/user";
-import { useRoute, useRouter } from 'vue-router'
-import { useAppStore } from "@/pinia"
-import { storeToRefs } from "pinia"
-import { computed, } from 'vue'
-import { setUserAuthority } from '@/api/user'
-import { fmtTitle } from "@/utils/fmtRouterTitle";
-import gvaAside from "@/view/layout/aside/index.vue"
-const userStore = useUserStore();
-const router = useRouter()
-const route = useRoute()
-const appStore = useAppStore()
-const { device , config } = storeToRefs(appStore)
-const isMobile = computed(() =>{
-  return device.value  === 'mobile'
-})
-const toPerson = () => {
-  router.push({ name: "person" });
-};
-const matched = computed(() => route.meta.matched);
-
-
-const changeUserAuth = async (id) => {
-  const res = await setUserAuthority({
-    authorityId: id,
-  });
-  if (res.code === 0) {
-    window.sessionStorage.setItem("needCloseAll", "true");
-    window.sessionStorage.setItem("needToHome", "true");
-    window.location.reload();
-  }
-};
-
-
-</script>
-
-<style scoped lang="scss">
-
-</style>
->>>>>>> wx-tally
+<style scoped lang="scss"></style>
